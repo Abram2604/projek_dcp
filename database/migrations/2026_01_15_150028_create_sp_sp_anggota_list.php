bda4c076313de0592de6b59283d8e1d35c422472
Abram <abdulramalah26@gmail.com>
2026-01-15 15:05:46 +0700

    perubahan sp

--- database/migrations/2026_01_15_150028_create_sp_sp_anggota_list.php
+++ database/migrations/2026_01_15_150028_create_sp_sp_anggota_list.php
@@ -7,11 +7,8 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // 1. Hapus SP lama jika ada (untuk menghindari error 'already exists')
-        DB::unprepared("DROP PROCEDURE IF EXISTS `sp_anggota_list`");
-
-        // 2. Buat SP Baru
-        DB::unprepared("
+        DB::unprepared(<<<'SQL'
+DROP PROCEDURE IF EXISTS `sp_anggota_list`;
 CREATE PROCEDURE `sp_anggota_list` (
     IN `p_search` VARCHAR(100),
     IN `p_status_aktif` TINYINT
@@ -25,9 +22,11 @@ BEGIN
         a.nomor_hp,
         a.string_kode_qr,
         a.status_aktif,
-        j.nama_jabatan,
-        d.nama_divisi,
-        j.level_akses
+        a.id_jabatan,   
+        a.id_divisi,    
+        j.nama_jabatan, 
+        d.nama_divisi,  
+	j.level_akses       
     FROM Anggota a
     LEFT JOIN Jabatan j ON a.id_jabatan = j.id
     LEFT JOIN Divisi d ON a.id_divisi = d.id
@@ -39,11 +38,16 @@ BEGIN
          a.username LIKE CONCAT('%', p_search, '%'))
     ORDER BY a.nama_lengkap ASC;
 END
-        ");
+SQL
+        );
     }
 
     public function down(): void
     {
-        DB::unprepared("DROP PROCEDURE IF EXISTS `sp_anggota_list`");
+        // Logic rollback (jika ada)
+        DB::unprepared(<<<'SQL'
+DROP PROCEDURE IF EXISTS `sp_anggota_list`;
+SQL
+        );
     }
 };
